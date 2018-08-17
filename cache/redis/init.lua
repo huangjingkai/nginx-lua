711df80e1a77c0d4171fbba4758cec9238c1c77b
huangjingkai <huangjingkai@huawei.com>
2018-08-17 18:33:11 +0800

    Complete business debugging

--- cache/redis/init.lua
+++ cache/redis/init.lua
@@ -1,23 +1,20 @@
 -- Redis Cache module: Transparent subrequest-based caching layout for arbitrary nginx locations. 
--- Auth: huangjingkai
--- Date: 201X/04/23
+-- Auth: huangjingkai#foxmail.com
+-- Date: 1398241561
 
+-- [Configuration Start]
 g_cache = {}
--- Server host and port.
-g_cache.redis_host = "127.0.0.1"
-g_cache.redis_port = 6379
-g_cache.redis_password = "1qaz@WSX"
-
-local redis_path = '/usr/local/NSP/etc/redis'
-local file_uri_cache = redis_path .. '/uri_cache'
-
 --[[
-g_cache.err_return_page: failed page. 
+g_cache.err_return_page, There are several types of pages returned after failure:
+	1] connects to the redis server [error] to prevent the large flow impact on the back end and return to the page directly.
+	2] connects to the redis server [normal], but gets the key value [exception] and returns directly to the area.
+	3) Connect to the redis server [normal], but loop g_cache. search_count times, get the key value [nil] and return to the page directly.
+	4] to the backend request page, return the code is not 200 OK (currently only caching the page returning to 200ok).
 ]]
 g_cache.err_return_page = "/503.html"
 
 -- Sets the timeout (in ms) protection for subsequent operations, including the connect method.
-g_cache.redis_timeout = 1000
+g_cache.redis_timeout = 2000
 
 --[[
 Puts the current Redis connection immediately into the ngx_lua cosocket connection pool.
@@ -26,23 +23,23 @@ You can specify the max idle timeout (in ms) when the connection is in the pool 
 g_cache.redis_max_idle_timeout = 60000
 g_cache.redis_pool_size = 100
 --[[
-g_cache.search_count: 连接redis服务器，如果 key 值不存在的情况，并且不是该key值第一次查询的情况下，循环获取的次数。
-g_cache.search_sleep: 连接redis服务器，如果 key 值不存在的情况，并且不是该key值第一次查询的情况下，每次循环需要等待的时间。
-g_cache.search_dict_exptime: 首次获取 key 值，至少在该值之后，才会进行下一次查询。
+g_cache.search_count: Connect to the redis server, if the key value does not exist, and is not the first time the key value is queried, the number of loops obtained.
+g_cache.search_sleep: Connect to the redis server. If the key value does not exist and is not the first time the key value is queried, the wait time for each loop is required.
+g_cache.search_dict_exptime: Gets the key value for the first time, at least after that value, before the next query.
 ]]
 g_cache.search_count = 5
 g_cache.search_sleep = 0.4
 g_cache.search_dict_exptime = 1
 
--- 默认的uri超时时间，如果配置文件没有设置就使用这个值。
-g_cache.uri_default_exptime = 5
+-- The default URI timeout time is used if the configuration file is not set.
+g_cache.uri_default_exptime = 600
 
--- 需要删除的 请求 的头域。
+-- The header domain of the request to be deleted.
 g_cache.req_header={
 ["Accept-Encoding"]=1,
 }
 
--- 响应 头域中包含以下几个，属于 delete 则删除，属于 no-cache 则该响应不缓存。
+-- The response header field contains the following, deletes if it belongs to delete, and does not cache if it belongs to no-cache.
 g_cache.resp_header={
 ["ETag"]="delete",
 ["Content-Length"]="delete",
@@ -51,7 +48,17 @@ g_cache.resp_header={
 ["Cookie"]="delete",
 ["Date"]="delete",
 ["Vary"]="delete",
+["X-CACHE-S"]="no-cache"
 }
+
+-- The expiration time of redundant cache, default 3600s
+g_cache.stale_cache_expire_time = 3600
+
+-- Internally fixed string used to cache dynamically replace special content.
+g_cache.internal_replace_str="<INTERNAL_REPLACE_STRING>"
+
+-- redis Cache log level
+g_cache.log_level=ngx.NOTICE
 
 -- [Configuration End]
 
@@ -69,20 +76,112 @@ function redis_string_split(src, sub)
   return string.sub(src, 1, i-1), string.sub(src, j+1, -1)
 end
 
-function string_split(s, p)
-  local rt= {}
-  string.gsub(s, '[^'..p..']+', function(w) table.insert(rt, w) end )
-  return rt
+function redis_get_stale_key(key)
+  if key == nil then return; end
+  local key_stale=key .. "_stale"
+  return key_stale
 end
 
-function ngx_save_set(dict,key,value)
-  success, err, forcible = dict:set(key,value)
-  if not success then
-    ngx.log(ngx.ERR, '[LUA] nginx dict set error, ', err)
+function redis_string_replace(src,repl_old,repl_new)
+  if src ~= nil and repl_old ~=nil and repl_new ~= nil then
+    local str_len=string.len(src)
+    local i,j=string.find(src,repl_old)
+    if i ~= nil then
+      if i == 1 then
+        return repl_new .. string.sub(src,j+1,-1)
+      elseif j == str_len then
+        return string.sub(src,1,i-1) .. repl_new
+      else
+        return string.sub(src,1,i-1) .. repl_new .. string.sub(src,j+1,-1)
+      end
+    else
+	ngx.log(g_cache.log_level,"[X-DCS]cant find repl_old=",repl_old,",repl_new=",repl_new)
+    end
   end
+  return src;
+  --return string.gsub(src,repl_old,repl_new);
 end
 
-ngx.shared.dict_cache_uri_search_flag:flush_all()
-ngx.shared.dict_cache_uri_exptime:flush_all()
+--[[
+Description     : try to find cache from redis and assemble HTTP response
+Input Parameter : redis_inst,redis instance
+				  key,specify cache key
+				  expire, User expected expiration time
+				  is_stale_cache, Does the user expect to make history caching?
+                  status,specify ngx.header['X-DCS-STATUS']
+Return Value    : nil
+Date                    : 1407227161
+Author                  : lvguanglin                               
+]]
+function try_to_get_resp_from_redis(redis_inst,keys,expire,is_stale_cache,status)
+-- calculates the difference between the maximum time of historical caching and the expected expiration time of users.
+  local stale_time = 0
+  if is_stale_cache and expire < g_cache.stale_cache_expire_time then
+	stale_time = g_cache.stale_cache_expire_time - expire
+  end
+  
+  --BEGIN: Modified By lvguanglin, If the expiration time is stored as the historical cache expiration time, the Lua script is executed on Redis using the eval command to check whether the specified record has exceeded the user's expected expiration time. 1407227161
+  local res
+  local err
+  if stale_time > 0 then
+	res, err = redis_inst:eval("local left_t=redis.call('ttl',KEYS[1]) if left_t > 0 and left_t > tonumber(KEYS[2]) then return redis.call('get',KEYS[1]) else return nil end",2,keys,stale_time)
+  else
+	res, err = redis_inst:get(keys)
+  end
+  if not res then
+	ngx.log(ngx.ERR, "[X-DCS] failed to get keys: ", keys,",err:",err)
+	return false
+  end
+  --END
 
-ngx.log(ngx.WARN, '[CACHE] nginx redis script start.')
+  if res ~= ngx.null and type(res) == "string" then
+    local values = res
+    local headers, body = redis_string_split(values, '\n\n')
+
+    if headers ~= nil and body ~= nil then
+      local rt= {}
+      string.gsub(headers, '[^'..'\n'..']+', function(w) table.insert(rt, w) end )
+
+      for k,v in pairs(rt) do
+          local header_name, header_value = redis_string_split(v, ':')
+          ngx.header[header_name] = header_value
+      end
+
+--BEGIN: Added by lvguanglin, When the cache hits, it determines whether cache replacement is necessary. 1407227161
+      local callback_val = ngx.var.callback_val
+      if callback_val ~= nil and callback_val ~= ngx.null and 0 < string.len(callback_val) then
+        ngx.log(g_cache.log_level,"[X-DCS]Redis Hit,need to replace before give body to user,callback_val=",callback_val)
+        body=redis_string_replace(body,g_cache.internal_replace_str,callback_val)
+      end
+--END
+
+      local ok, err = redis_inst:set_keepalive(g_cache.redis_max_idle_timeout, g_cache.redis_pool_size)
+      if not ok then
+        ngx.log(ngx.ERR, "[X-DCS] failed to set keepalive", err )
+      end
+
+      ngx.header['X-DCS-STATUS'] = status
+      ngx.header["pragma"]="no-cache"
+      ngx.header["cache-control"]="no-cache, no-store, max-age=0"
+      ngx.print(body)
+      ngx.exit(ngx.HTTP_OK)
+    end
+  end
+  return true
+end
+
+--[[
+Description     : try to find history cache from redis
+Input Parameter : redis_inst, redis instance 
+				  key,specify cache key
+                  status,specify ngx.header['X-DCS-STATUS']
+Return Value    : nil
+Date                    : 1407227161
+Author                  : lvguanglin                               
+]]
+function try_to_get_stale_resp_from_redis(redis_inst,key,status)
+-- Here we want to read the history buffer, so the expired time passes the expiration time of the historical cache.
+  return try_to_get_resp_from_redis(redis_inst,key,g_cache.stale_cache_expire_time,true,status)
+end
+
+ngx.log(ngx.WARN, '[X-DCS] nginx redis script start.')
--- cache/redis/redis.lua
+++ cache/redis/redis.lua
@@ -1,80 +1,93 @@
 -- Redis Cache module: Transparent subrequest-based caching layout for arbitrary nginx locations. 
--- Auth: huangjingkai
--- Date: 201X/04/23
+-- Auth: huangjingkai#foxmail.com
+-- Date: 1398241561
 
-local req_uri = ngx.var.uri
-if ngx.var.args ~= nil then req_uri = req_uri .. '?' .. ngx.var.args; end
-local real_uri = ngx.var.host .. string.sub(ngx.var.uri, 7, -1)
-local real_request_uri = string.sub(req_uri,7,-1)
+--[[
+NGINX parameters:
+  1. User define parameters:
+  *** Required ) ngx.var.redis_host
+  *** Required ) ngx.var.redis_port
+  *** Required ) ngx.var.redis_password
+  *** Required ) ngx.var.cache_key
+  * option ) ngx.var.exptime, user define, default: 600s
+  * option ) ngx.var.callback_val, user define, default : null
+  * option ) ngx.var.do_stale_cache, user define, default: 
 
-local keys = ngx.var.cache_key
-if keys == '' or keys == nil then
-    ngx.log(ngx.ERR,"[CACHE]cache_key is nil,wil goto reverse_proxy")
-    ngx.header['X-RDS-STATUS'] = 'MISS'
-    return ngx.exec('/reverse-proxy')
+  2. System parameters:
+  * ngx.var.request_method
+  * ngx.var.request_body
+  * ngx.var.content_length
+]]
+
+
+-- Get the value of ngx.var.cache_backend
+local backend = "/reverse-proxy"
+if ngx.var.cache_backend ~= nil and ngx.var.cache_backend ~= "" then
+    backend = "/" .. ngx.var.cache_backend
 end
 
---
---  Connect to the redis server.
---
+local keys = ngx.var.cache_key
+if keys == nil or keys == "" or keys == "initial" then
+    ngx.log(ngx.ERR,"[X-DCS]cache_key is nil,wil goto bacnend: ", backend)
+    ngx.header['X-DCS-STATUS'] = 'MISS'
+    return ngx.exec(backend)
+end
 
+
+-- Get user behavior, do you expect to do historical caching.
+local is_stale_cache = true 
+local stale_cache = ngx.var.do_stale_cache
+if stale_cache ~= nil and stale_cache ~= "" and string.len(stale_cache) > 0 then
+	is_stale_cache = (string.lower(stale_cache) == "yes" and true) or false
+end
+
+-- Get user expected cache time
+local exptime = tonumber(ngx.var.exptime)
+if exptime == nil or exptime <= 0 then
+	ngx.log(g_cache.log_level, "[X-DCS] Use default exptime, keys=", keys)
+	exptime = g_cache.uri_default_exptime
+end
+
+--  Connect to the redis server.
 local redis = require "resty.redis"
 local red = redis:new()
 red:set_timeout(g_cache.redis_timeout) -- 1 sec
 
 -- connect to a unix domain socket file listened by a redis server:
+-- local ok, err = red:connect("unix:/usr/local/NSP/etc/redis/redis.sock", {pool="mypool"})
 local ok, err = red:connect(ngx.var.redis_host, ngx.var.redis_port, {pool="mypool"})
 if not ok then
-  ngx.log(ngx.ERR, "[CACHE] redis server failed to connect: ", ngx.var.redis_host, ngx.var.redis_port)
-  ngx.header['X-CACHED-STATUS'] = 'DOWN'
-  return ngx.exec(g_cache.err_return_page)
+	ngx.log(ngx.ERR, "[X-DCS] Redis server failed to connect: ", g_cache.redis_host, g_cache.redis_port)
+	ngx.header['X-DCS-STATUS'] = 'DOWN'
+	ngx.header["pragma"]="no-cache"
+	ngx.header["cache-control"]="no-cache, no-store, max-age=0"
+	ngx.log(ngx.ERR,"[X-DCS]can not connect to redis ,wil goto backend: " , backend, "err: ", err)
+    return ngx.exec(backend)
 end
 
 local res, err = red:auth(ngx.var.redis_password)
 if not res then
-  ngx.log(ngx.ERR, "[CACHE] auth failed: ", ngx.var.redis_host, ":", ngx.var.redis_port)
-  ngx.header['X-CACHED-STATUS'] = 'DOWN'
-  ngx.header["pragma"]="no-cache"
-  ngx.header["cache-control"]="no-cache, no-store, max-age=0"
-  return ngx.exec('/reverse-proxy')
+	ngx.log(ngx.ERR, "[CACHE] auth failed: ", ngx.var.redis_host, ":", ngx.var.redis_port)
+	ngx.header['X-CACHED-STATUS'] = 'DOWN'
+	ngx.header["pragma"]="no-cache"
+	ngx.header["cache-control"]="no-cache, no-store, max-age=0"
+	ngx.log(ngx.ERR,"[X-DCS] can not connect to redis , passwordd is wrong, wil goto backend: " , backend, ",err: ", err)
+    return ngx.exec(backend)
 end
 
 local count = g_cache.search_count
-local loop
+local loop loop_cnt=0
 for loop = 1,count,1 do
-  local res, err = red:get(keys)
-  if not res then
-    ngx.log(ngx.ERR, "[CACHE] failed to get keys: ", keys)
-    ngx.header['X-CACHED-STATUS'] = 'MISS' 
+--BEGIN：Modified By lvguanglin, Encapsulated as functions, the number of cycles needs to be counted individually by loop_cnt. 1407486361
+  loop_cnt = loop
+  if not try_to_get_resp_from_redis(red,keys,exptime,is_stale_cache,'HIT') then
+    ngx.header['X-DCS-STATUS'] = 'MISS'
+    ngx.header["pragma"]="no-cache"
+    ngx.header["cache-control"]="no-cache, no-store, max-age=0"
     return ngx.exec(g_cache.err_return_page)
   end
-
-  if res ~= ngx.null and type(res) == "string" then
-    local values = res
-
-    local headers, body = redis_string_split(values, '\n\n')
-
-    if headers ~= nil and body ~= nil then
-      local rt= {}
-      string.gsub(headers, '[^'..'\n'..']+', function(w) table.insert(rt, w) end )
-
-      for k,v in pairs(rt) do
-          local header_name, header_value = redis_string_split(v, ':')
-          ngx.header[header_name] = header_value
-      end
-      
-      local ok, err = red:set_keepalive(g_cache.redis_max_idle_timeout, g_cache.redis_pool_size)
-      if not ok then
-        ngx.log(ngx.ERR, "[CACHE] failed to set keepalive", err )
-      end
-
-      ngx.header['X-CACHED-STATUS'] = 'HIT'
-
-      ngx.print(body)
-      ngx.exit(ngx.HTTP_OK)
-    end
-  end
-
+--END
+  
   -- If the first time, then break to get reply from backend.
   if not ngx.shared.dict_cache_uri_search_flag:get(keys) then
     ngx.shared.dict_cache_uri_search_flag:set(keys, 1, g_cache.search_dict_exptime)
@@ -82,20 +95,25 @@ for loop = 1,count,1 do
   end
 
   ngx.sleep(g_cache.search_sleep)
-
 end
 
-if loop == count then
-  ngx.header['X-CACHED-STATUS'] = 'MISS'
+if loop_cnt == count then
+--BEGIN：Added By lvguanglin, If the cache exceeds a certain number of times, if history cache is made, try to query the history cache. 1407486361
+  if is_stale_cache then
+	try_to_get_stale_resp_from_redis(red,keys,'STALE')
+  end
+--END  
+  ngx.header['X-DCS-STATUS'] = 'MISS'
+  ngx.header["pragma"]="no-cache"
+  ngx.header["cache-control"]="no-cache, no-store, max-age=0"
   return ngx.exec(g_cache.err_return_page)
 end
 
 
-ngx.log(ngx.WARN, "[CACHE] Search from backend, key=", keys)
+ngx.log(g_cache.log_level, "[X-DCS] Search from backend, key=", keys)
 
 
 -- query from the backend.
-
 for k,v in pairs(g_cache.req_header) do
   ngx.req.clear_header(k)
 end
@@ -106,36 +124,44 @@ if ngx.var.request_method == "POST" then
    capture_options.method=ngx.HTTP_POST
    local req_body = ngx.var.request_body
    if not req_body and ngx.var.content_length ~= "0" then
-    local file = ngx.req.get_body_file()
-  if file then 
-      local fp = io.open(file,"r")
-      if fp then fp:seek("set",0) req_body = fp:read("*a") fp:close() end
-  end
+   	local file = ngx.req.get_body_file()
+	if file then 
+	    local fp = io.open(file,"r")
+	    if fp then fp:seek("set",0) req_body = fp:read("*a") fp:close() end
+	end
    end
    if not req_body then 
-  ngx.log(ngx.ERR,"[CACHE]post request but doesnt has request_body,content_length:"..tostring(ngx.var.content_length))
-        ngx.header['X-CACHED-STATUS'] = 'MISS'
-    return ngx.exec('/reverse-proxy')
+	ngx.log(ngx.ERR,"[X-DCS]post request but doesnt has request_body,content_length:"..tostring(ngx.var.content_length))
+        ngx.header['X-DCS-STATUS'] = 'MISS'
+  	return ngx.exec(backend)
    end
    capture_options.body=req_body
-   ngx.log(g_cache.log_level,"[CACHE]it's post request,cache_key is " ..keys)
+   ngx.log(g_cache.log_level,"[X-DCS]it's post request,cache_key is " ..keys)
 end
 
-local res_backend = ngx.location.capture('/reverse-proxy',capture_options)
+local res_backend = ngx.location.capture(backend,capture_options)
 if res_backend.status ~= ngx.HTTP_OK then
   ngx.shared.dict_cache_uri_search_flag:delete(keys)
-  ngx.log(ngx.ERR,"[CACHE]response status is not ok,is "..res_backend.status)
-  ngx.header['X-CACHED-STATUS'] = 'MISS'
-  return ngx.exec('/reverse-proxy')
+--BEGIN：Added By lvguanglin, When the backend is down, a history buffer is attempted to query the history cache. 1407486361
+  if is_stale_cache then
+	  if not try_to_get_stale_resp_from_redis(red,keys,'STALE') then
+		ngx.header['X-DCS-STATUS'] = 'MISS'
+		ngx.header["pragma"]="no-cache"
+		ngx.header["cache-control"]="no-cache, no-store, max-age=0"
+		return ngx.exec(g_cache.err_return_page)
+	  end
+  end
+--END
+  ngx.log(ngx.ERR,"[X-DCS]response status is not ok,is ", res_backend.status, ". backend: ", backend)
+  ngx.header['X-DCS-STATUS'] = 'MISS'
+  return ngx.exec(backend)
 end
 
-ngx.header['X-CACHED-STATUS'] = 'STORE'
 
 -- save to the end.
 local headers =''
 local is_store = true
 for k,v in pairs(res_backend.header) do
-
   if g_cache.resp_header[k] == "no-cache" then
     is_store = false
     break
@@ -148,27 +174,47 @@ for k,v in pairs(res_backend.header) do
 end
 
 if is_store == true then
+  ngx.header['X-DCS-STATUS'] = 'STORE'
   local body = res_backend.body
+
+  --BEGIN: Added by lvguanglin, If there is a specific string, it is necessary to process the back-end response first and then store it in redis. 1407486361
+  local callback_v=ngx.var.callback_val
+  if callback_v ~= nil and callback_v ~= ngx.null and 0 < string.len(callback_v) then
+    ngx.log(g_cache.log_level,"[X-DCS]need to replace before save body to redis,callback_v=",callback_v)
+    body=redis_string_replace(body,callback_v,g_cache.internal_replace_str)  
+  end
+  --END
+
   local values = headers .. '\n' .. body
-  local exptime = ngx.shared.dict_cache_uri_exptime:get(real_uri)
+  local exptime = tonumber(ngx.var.exptime)
   if exptime == nil or exptime <= 0 then
-    ngx.log(ngx.WARN, "[CACHE] Use default exptime, uri=", real_uri)
+    ngx.log(g_cache.log_level, "[X-DCS] Use default exptime, keys=", keys)
     exptime = g_cache.uri_default_exptime
   end
-
-  local res, err = red:set(keys, values, "EX", exptime)
-  if not res then
-    ngx.log(ngx.ERR, "[CACHE] failed to set value: ", values)
+  
+  --BEGIN：Added By lvguanglin, When history caching is enabled, cache expiration times are set to larger values in exptime and g_cache. stale_cache_expire_time, add 10 minutes. 1407486361
+  local stale_exptime = g_cache.stale_cache_expire_time
+  if is_stale_cache and stale_exptime ~= nil and exptime < stale_exptime then
+	exptime = stale_exptime
   end
+  
+  -- Explosion proof, add 10 minutes
+  exptime = exptime + 600
+  local res, err = red:setex(keys,exptime,values)
+  if not res then
+    ngx.log(ngx.ERR, "[X-DCS] failed to set value: ", values)
+  end
+--END
 else
-  ngx.log(ngx.WARN, "[CACHE] cache miss, key=", real_uri)
+  ngx.header['X-DCS-STATUS'] = 'PASS'
+  ngx.log(g_cache.log_level, "[X-DCS] cache miss, keys=", keys)
 end
 
 -- put it into the connection pool of size 100,
 -- with 60 seconds max idle time
 local ok, err = red:set_keepalive(g_cache.redis_max_idle_timeout, g_cache.redis_pool_size)
 if not ok then
-  ngx.log(ngx.ERR, "[CACHE] failed to set keepalive: ", err)
+  ngx.log(ngx.ERR, "[X-DCS] failed to set keepalive: ", err)
 end
 
 ngx.print(res_backend.body)
